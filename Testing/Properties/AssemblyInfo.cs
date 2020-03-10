3dabf7a52baeafec6e1cbc7b161fe4c1f5881506
Akash D' Cross <1748930@dundeeandangus.ac.uk>
2020-03-10 11:00:29 +0000

    simpleCalcu

---
+++ Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("702db97a-f92d-4be3-a00b-6bdd88f13ef7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Testing1/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
+
+namespace Testing1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MainPage Test1 = new MainPage();
+            Test1.
+            
+        }
+    }
+}
